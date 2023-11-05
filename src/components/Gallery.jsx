172e07262b28af7a62c42989efd953e56779e0fa
1Md-Rakibul-Islam <rakibul.islam.developer@gmail.com>
2023-11-05 15:14:23 -0800

    feat: checkbox style completed

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,6 +5,7 @@ const Gallery = ({ images }) => {
   const [draggedIndex, setDraggedIndex] = useState(null);
   const [galleryImages, setGalleryImages] = useState(images);
   const [selectedImages, setSelectedImages] = useState([]);
+  const [mouseHoveredImg, setMouseHoveredImg] = useState();
 
   const handleDragStart = (index) => {
     setDraggedIndex(index);
@@ -28,9 +29,14 @@ const Gallery = ({ images }) => {
     const isSelected = selectedImages.includes(index);
     if (isSelected) {
       setSelectedImages(selectedImages.filter((i) => i !== index));
+      setMouseHoveredImg("");
     } else {
       setSelectedImages([...selectedImages, index]);
     }
+  };
+
+  const handleHover = (index) => {
+    setMouseHoveredImg(index);
   };
 
   const handleDeleteSelected = () => {
@@ -39,42 +45,38 @@ const Gallery = ({ images }) => {
     );
     setGalleryImages(updatedImages);
     setSelectedImages([]);
-  };
-
-  const handleSetFeatureImage = () => {
-    if (selectedImages.length > 0) {
-      const featureImageIndex = selectedImages[0]; // Pretend the first selected image as the feature image
-      const updatedImages = galleryImages.map((img, index) => {
-        return {
-          ...img,
-          isFeatureImage: index === featureImageIndex,
-        };
-      });
-      setGalleryImages(updatedImages);
-      setSelectedImages([]);
-    }
+    setMouseHoveredImg("");
   };
 
   return (
     <div className="max-w-4xl bg-white shadow-2xl lg:rounded-lg md:rounded-md rounded-sm">
-      {selectedImages && (
-        <div className="flex justify-between items-center lg:px-8 md:px-7 sm:px-6 px-5 lg:py-6 md:py-7 py-5 border-b-2 border-gray-300 font-bold">
-          <div className="flex items-center gap-3">
-            <input type="checkbox" name="select-all" id="" />
-            <h6 className="text-black text-xl">
-              {selectedImages?.length} File Selected
-            </h6>
-          </div>
-          {selectedImages?.length > 0 && (
+      <div className="flex justify-between items-center lg:px-8 md:px-7 sm:px-6 px-5 lg:py-6 md:py-7 py-5 border-b-2 border-gray-300 font-bold">
+        {selectedImages?.length > 0 ? (
+          <>
+            <div className="flex items-center gap-3">
+              <input
+                onChange={() => setSelectedImages([])}
+                className="px-2 py-1 !z-50 h-5 w-5"
+                type="checkbox"
+                name="select-all"
+                id=""
+                checked
+              />
+              <h6 className="text-black text-xl">
+                {selectedImages?.length} File Selected
+              </h6>
+            </div>
             <button
               onClick={handleDeleteSelected}
               className="text-red-600 text-base "
             >
               Delete file
             </button>
-          )}
-        </div>
-      )}
+          </>
+        ) : (
+          <h6 className="text-black text-xl font-bold">Gallery</h6>
+        )}
+      </div>
 
       <div className="grid grid-rows-3 grid-cols-5 gap-4 lg:p-8 md:p-7 sm:p-6 p-5">
         {galleryImages?.map((image, index) => (
@@ -83,24 +85,24 @@ const Gallery = ({ images }) => {
             onDragStart={() => handleDragStart(index)}
             onDragOver={(event) => handleDragOver(event, index)}
             onDrop={() => handleDrop(index)}
+            onMouseOver={() => handleHover(index)}
             draggable
             className={`${
               index === 0 ? "row-span-2 col-span-2" : "col-span-1"
-            } hover:brightness-50 cursor-grab ease-in-out duration-500 rounded-lg border-2 border-gray-300`}
+            } ${
+              selectedImages?.includes(index) && "opacity-50"
+            } hover:brightness-50 cursor-grab ease-in-out duration-500 rounded-lg border-2 border-gray-300 relative`}
           >
-            <div
-              className={`relative ${
-                selectedImages?.includes(index) ? "border border-blue-500" : ""
-              }`}
-              onClick={() => handleToggleSelect(index)}
-            >
-              <Image src={image} />
-              {selectedImages?.includes(index) && (
-                <div className="absolute top-3 left-3 bg-blue-500 text-white rounded-full px-2 py-1">
-                  ✓
-                </div>
-              )}
-            </div>
+            <Image src={image} />
+            {(selectedImages?.includes(index) || mouseHoveredImg === index) && (
+              <input
+                onChange={() => handleToggleSelect(index)}
+                className={`absolute top-3 left-3 px-2 py-1 !z-50 h-5 w-5`}
+                type="checkbox"
+                name="select"
+                checked={selectedImages?.includes(index) ? true : false}
+              />
+            )}
           </div>
         ))}
       </div>
